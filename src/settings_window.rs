9fee208f5f3ee178dfa26802d89a3cb1743c8271
Boas Kalma <19141319@student.hhs.nl>
2022-11-01 22:08:51 +0100

    code now opens settings window

--- src/settings_window.rs
+++ src/settings_window.rs
@@ -1,17 +1,27 @@
-use iced::{ Application, executor };
+use iced::{ Application, executor, Command, Settings };
 
 use crate::screen_saver_settings::ScreenSaverSettings;
-
-#[derive(Default)]
-pub struct SettignsWindow {
-    schreen_saver_setting: ScreenSaverSettings,
-}
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     Update,
     Default,
     Close,
+}
+#[derive(Default)]
+pub struct SettignsWindow {
+    schreen_saver_setting: ScreenSaverSettings,
+}
+
+impl SettignsWindow {
+    pub fn start() {
+        let schreen_saver_settings = ScreenSaverSettings::default();
+        let window_result = SettignsWindow::run(Settings::with_flags(schreen_saver_settings));
+        match window_result {
+            Ok(result) => result,
+            Err(error) => { panic!("Cant open window: {}", error.to_string()) }
+        }
+    }
 }
 
 impl Application for SettignsWindow {
@@ -21,11 +31,12 @@ impl Application for SettignsWindow {
     type Flags = ScreenSaverSettings;
 
     fn new(flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
-        todo!()
+        let output = SettignsWindow { schreen_saver_setting: flags };
+        (output, Command::none())
     }
 
     fn title(&self) -> String {
-        todo!()
+        String::from("STHIL - settings")
     }
 
     fn update(&mut self, message: Self::Message) -> iced::Command<Self::Message> {
@@ -44,8 +55,21 @@ impl Application for SettignsWindow {
     fn view(&mut self) -> iced::Element<'_, Self::Message> {
         todo!()
     }
+
+    fn background_color(&self) -> iced::Color {
+        iced::Color::from_rgb(1.0, 1.0, 1.0)
+    }
+
+    fn scale_factor(&self) -> f64 {
+        1.0
+    }
+
+    fn mode(&self) -> iced::window::Mode {
+        iced::window::Mode::Windowed
+    }
 }
 
 pub fn show_settings() {
-    println!("options")
+    println!("options");
+    SettignsWindow::start();
 }
