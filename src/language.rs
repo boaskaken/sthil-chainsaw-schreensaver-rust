71385d481c29bf1c3d1b348f5b450382e4d5f907
Boas Kalma <boas.kalma@planet.nl>
2022-11-29 19:19:15 +0100

    tryed to implement languages

--- src/language.rs
+++ src/language.rs
@@ -1,5 +1,27 @@
-pub trait Language {
-    fn hallo(&self) -> String;
+pub struct LanguageBox {
+    language: Box<dyn Language>,
+}
+
+impl LanguageBox {
+    pub fn new(input: &str) -> LanguageBox {
+        let language = Self::language_from_string(input);
+        LanguageBox { language }
+    }
+    pub fn set_language(&mut self, input: &str) {
+        let language = Self::language_from_string(input);
+        self.language = language;
+    }
+
+    fn language_from_string(input: &str) -> Box<dyn Language> {
+        match input {
+            "nl_NL" => {
+                return Box::new(Dutch {});
+            }
+            _ => {
+                return Box::new(English {});
+            }
+        }
+    }
 }
 
 pub fn language_from_string(input: &str) -> Box<dyn Language> {
@@ -13,17 +35,20 @@ pub fn language_from_string(input: &str) -> Box<dyn Language> {
     }
 }
 
+pub trait Language {
+    fn hallo(&self) -> String;
+}
+
 struct Dutch {}
 impl Language for Dutch {
     fn hallo(&self) -> String {
-        return String::from("I am a test String");
+        return String::from("Ik ben een test string.");
     }
 }
 
 struct English {}
-
 impl Language for English {
     fn hallo(&self) -> String {
-        todo!()
+        return String::from("I am a test String.");
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ mod screen_saver_settings;
 mod language;
 
 use crate::language::Language;
+use crate::language::LanguageBox;
 use crate::language::language_from_string;
 use crate::saw::*;
 use crate::settings_window::*;
@@ -16,10 +17,16 @@ use crate::project_error::ProjectError;
 use crate::error_window::*;
 
 fn main() {
-    const current_locale: String = get_locale().unwrap_or_else(|| String::from("en-US"));
-    static language: Box<dyn Language> = language_from_string(current_locale.as_str());
-    print!("{}", current_locale);
-    panic!("i a dont want to anymore.");
+    let locale_option = get_locale() ;
+    let current_locale: String = match locale_option{
+        Some(s) => s,
+        None => String::from("en-US"),
+    };
+
+    //static language : LanguageBox = LanguageBox::new(current_locale);
+    let language: Box<dyn Language> = language_from_string(current_locale.as_str());
+    //print!("{}", current_locale);
+    //panic!("i a dont want to anymore.");
     let args: Vec<String> = env::args().collect();
     match get_argument(&args) {
         Ok(argument) =>
